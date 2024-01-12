03def57fa5248763a6ee4d553b7e1070b9617eaf
Khai Lam Nguyen <khailam.nguyen@mail.utoronto.ca>
2024-01-12 15:17:54 -0500

    Initialize clone

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,34 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+import NavBar from "@/components/NavBar";
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+  title: "Breadit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <NavBar />
+        <div className="container max-w-7x1 mx-auto h-full pt-12">
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
